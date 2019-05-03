4523f685add9f3eb4cba720ecd5bc0a7529ef0fb
Michael Knudson <m.knudso@gmail.com>
2019-05-03 12:46:18 -0400

    Revert "add fitness function and hill climbing, and difficulty radio"
    This reverts commit 0d698b967c91f3ff97f3c9eb14580bcf5394ed64.

--- src/boardGenerator.js
+++ src/boardGenerator.js
@@ -2,15 +2,11 @@ function randomInt(max) {
     return Math.round(Math.random() * Math.floor(max));
   }
 
-// function weightedRandom(max, min=1) {
-//     // found in comments of https://stackoverflow.com/questions/8435183/generate-a-weighted-random-number
-//     // tends exponentially to min
-//     return max + 1 - Math.round(max / (Math.random() * max + min));
-// }
-
-// function grid2lin(pos, size) {
-//     return pos.row * size + pos.col;
-// }
+function weightedRandom(max) {
+    // found in comments of https://stackoverflow.com/questions/8435183/generate-a-weighted-random-number
+    // tends exponentially to min (hard-coded here to 1)
+    return max + 1 - Math.round(max / (Math.random() * max + 1));
+}
 
 function lin2grid(i, size) {
     return {
@@ -19,63 +15,39 @@ function lin2grid(i, size) {
     };
 }
 
-function isSuccessor(board, size, active, i) {
+function grid2lin(pos, size) {
+    return pos.row * size + pos.col;
+}
+
+function isNeighbor(board, size, active, i) {
     let sameColumn = ((i - active) % size === 0);
-    let sameRow = (Math.floor(i/size) === Math.floor(active/size));
+    let sameRow = (Math.floor(i/5) === Math.floor(active/5));
     let horizontalAway = (Math.abs(i-active) === board[active]);
     let verticalAway = (Math.abs(Math.floor(i / size) - Math.floor(active / size)) === board[active]);
     return ((sameRow && horizontalAway) || (sameColumn && verticalAway)); 
   }
 
-function fitness(gameBoard, size, difficulty) {
-    // fitness of board, fitness of 0 means no solutions.
-    let fitness = 0;
+function solvable(gameBoard, size) {
     let frontier = [];
     let explored = [];
-    let goal = gameBoard.length - 1;
-
-    let paths = {
-        0: [0],
-    }    
-
     frontier.push(0);
     let curr;
     while (frontier.length !== 0) {
         curr = frontier.shift();
         explored.push(curr);
-        let successors = [];
+        let neighbors = [];
         // TODO: optimize this
         for (let i = 0; i < gameBoard.length; i++) {
-            if (isSuccessor(gameBoard, size, curr, i)) {
-                successors.push(i);
+            if (isNeighbor(gameBoard, size, curr, i)) {
+                neighbors.push(i);
             }
         }
-        for (let successor of successors) {
-            // test for solvability
-            if (successor === goal) fitness = 1;
-            if (!explored.includes(successor) && !frontier.includes(successor)) {
-                frontier.push(successor);
-                paths[successor] =  paths[curr].concat(successor);
-            } 
+        for (let neighbor of neighbors) {
+            if (neighbor === gameBoard.length - 1) return true;
+            if (!explored.includes(neighbor) && !frontier.includes(neighbor)) frontier.push(neighbor);
         };
     }
-    if (fitness === 0) return fitness;
-
-    fitness *= paths[goal].length / difficulty;
-    // console.log(paths);
-    console.log(fitness);
-    if (fitness > 1) console.log(paths);
-
-    return fitness;
-}
-
-function getNeighbor(gameBoard) {
-    // randomly change a square's value to the value of another square.
-    // This bypasses the need to calculate the max permissible value.
-    // Swap with any square other than goal(choose from index 0 to length - 2), which is 0.
-    let neighbor = gameBoard.slice();
-    neighbor[randomInt(gameBoard.length - 1)] = neighbor[randomInt(gameBoard.length - 2)];
-    return neighbor;
+    return false;
 }
 
 function initGameBoard(size) {
@@ -88,32 +60,22 @@ function initGameBoard(size) {
         gameBoard[i] = randomInt(max - 1) + 1;
     }
 
-    gameBoard[gameBoard.length - 1] = 0;
-
     return gameBoard;
 }
 
-function boardGenerator(difficulty) {
-    let size = Math.floor(difficulty/3) + 3;
+function boardGenerator(size) {
     let gameBoard = initGameBoard(size);
-    let thisFit = fitness(gameBoard, size, difficulty);
-    while (thisFit <= 1) {
-        let neighbor = getNeighbor(gameBoard);
-        let neighborFitness = fitness(neighbor, size, difficulty)
-        if (neighborFitness > thisFit) gameBoard = neighbor;
-        thisFit = neighborFitness;
-    }
-    console.log(thisFit ? 'Board is solvable' : 'Board is impossible');
+    console.log(solvable(gameBoard, size));
     return gameBoard;
 }
 
-// const easyBoard = [3, 4, 3, 2, 4, 
-//                    2, 3, 3, 3, 2,
-//                    3, 3, 2, 1, 3,
-//                    3, 2, 1, 3, 1,
-//                    4, 4, 3, 2, 0];
+const easyBoard = [3, 4, 3, 2, 4, 
+                   2, 3, 3, 3, 2,
+                   3, 3, 2, 1, 3,
+                   3, 2, 1, 3, 1,
+                   4, 4, 3, 2, 0];
 
-// console.log(fitness(easyBoard, 5));
+// console.log(solvable(easyBoard, 5));
 
 // boardGenerator(3);
 
