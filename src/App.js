63d18b4075ceeef378dbeabbc69ffdd11c19b7aa
Michael Knudson <m.knudso@gmail.com>
2019-05-03 12:47:23 -0400

    see prev commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,57 @@ import Game from './Game.js'
 
 class App extends React.Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      difficulty: 4
+    }
+  }
+
+  handleOptionChange = e => {
+    console.log(`${e.target.value} has been selected!`);
+    this.setState( { difficulty: e.target.value } );
+    this.forceUpdate();
+  }
+
   render() {
+
     return (
       <div>
         Hello, world!
         <Game 
-          message="I'm hit!"
-          size="5"
+          difficulty={this.state.difficulty}
         />
+        <div>
+          <DifficultyRadio
+            difficulty={this.state.difficulty}
+            onChange={e => this.handleOptionChange(e)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class DifficultyRadio extends React.Component {
+  render() {
+    let buttons = [];
+    for (let i = 0; i < 10; i++) {
+      buttons.push(
+        <div key={i}>
+          <input
+            type="radio"
+            value={i + 1}
+            checked={this.props.difficulty == i + 1}
+            onChange={this.props.onChange}
+          />
+          {i + 1}
+        </div>
+      );
+    }
+    return(
+      <div>
+        {buttons}
       </div>
     );
   }
