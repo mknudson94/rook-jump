65df77225ddf4c8ac293e565b841ec8bcc14b714
Michael Knudson <m.knudso@gmail.com>
2019-05-03 10:12:56 -0400

    add solvable checker

--- src/boardGenerator.js
+++ src/boardGenerator.js
@@ -19,7 +19,38 @@ function grid2lin(pos, size) {
     return pos.row * size + pos.col;
 }
 
-function boardGenerator(size) {
+function isNeighbor(board, size, active, i) {
+    let sameColumn = ((i - active) % size === 0);
+    let sameRow = (Math.floor(i/5) === Math.floor(active/5));
+    let horizontalAway = (Math.abs(i-active) === board[active]);
+    let verticalAway = (Math.abs(Math.floor(i / size) - Math.floor(active / size)) === board[active]);
+    return ((sameRow && horizontalAway) || (sameColumn && verticalAway)); 
+  }
+
+function solvable(gameBoard, size) {
+    let frontier = [];
+    let explored = [];
+    frontier.push(0);
+    let curr;
+    while (frontier.length !== 0) {
+        curr = frontier.shift();
+        explored.push(curr);
+        let neighbors = [];
+        // TODO: optimize this
+        for (let i = 0; i < gameBoard.length; i++) {
+            if (isNeighbor(gameBoard, size, curr, i)) {
+                neighbors.push(i);
+            }
+        }
+        for (let neighbor of neighbors) {
+            if (neighbor === gameBoard.length - 1) return true;
+            if (!explored.includes(neighbor) && !frontier.includes(neighbor)) frontier.push(neighbor);
+        };
+    }
+    return false;
+}
+
+function initGameBoard(size) {
     let gameBoard = Array(size**2).fill(null);
     
     for (let i = 0; i < size**2; i++) {
@@ -31,5 +62,21 @@ function boardGenerator(size) {
 
     return gameBoard;
 }
+
+function boardGenerator(size) {
+    let gameBoard = initGameBoard(size);
+    console.log(solvable(gameBoard, size));
+    return gameBoard;
+}
+
+const easyBoard = [3, 4, 3, 2, 4, 
+                   2, 3, 3, 3, 2,
+                   3, 3, 2, 1, 3,
+                   3, 2, 1, 3, 1,
+                   4, 4, 3, 2, 0];
+
+// console.log(solvable(easyBoard, 5));
+
+// boardGenerator(3);
 
 export default boardGenerator;
